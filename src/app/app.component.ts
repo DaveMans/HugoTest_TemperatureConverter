626c1eda18501e27c72042df98f3a87f1c4b8792
David Mansilla <you@example.com>
2020-12-03 22:11:55 -0600

    Angular project with the task #2 of 7GUIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'temperatureConverterTest';
+  celsiusValue: number = 0;
+  fahrenheitValue: number = 0;
+  convert() {
+    if (this.celsiusValue !== null) {
+      this.fahrenheitValue = (this.celsiusValue * 9.0) / 5.0 + 32;
+    } else {
+      this.fahrenheitValue = 0;
+    }
+  }
 }
